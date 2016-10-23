9f4ecf7b67a3a4f36a772ee386c206361d3f5579
Ken <kenkjw@gmail.com>
2016-10-23 14:53:23 -0700

    Add basic models

---
+++ models/user.py
@@ -0,0 +1,38 @@
+from google.appengine.ext import db
+import bcrypt
+
+def hash_password(username, password):
+    return bcrypt.hashpw(username + password, bcrypt.gensalt())
+
+def check_password(username, password, hashed):
+    return bcrypt.checkpw(username + password, hashed)
+
+def users_key(group = 'default'):
+    return db.Key.from_path('users', group)
+
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    password = db.TextProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return cls.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = cls.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        return User(parent = users_key(),
+            username = name,
+            password = hash_password(name, password),
+            email = email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and check_password(name, pw, u.password):
+            return u
